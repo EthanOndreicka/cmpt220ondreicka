233ab03613e44fc472c2fbbe9943414a8060f12f
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2022-02-02 20:58:49 -0500

    Problem 1

---
+++ hw/1/Problem1.java
@@ -0,0 +1,15 @@
+import javax.sound.midi.Soundbank;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        
+        double a = 2;
+        double b = 5;
+        double c = 4;
+
+        double answer;
+        answer = (Math.pow(b, 2) - (4 * a * c));
+
+        System.out.println("The Discriminant of 2x^2 + 5x + 4 = 0 is: " + answer);
+    }
+}
