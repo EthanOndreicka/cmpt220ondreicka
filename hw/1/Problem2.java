03b1d9d97f161133b249bdee81c209769f7ad7be
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2022-02-02 21:05:57 -0500

    Problem 2

---
+++ hw/1/Problem2.java
@@ -0,0 +1,13 @@
+public class Problem2 {
+    public static void main(String[] args) {
+        
+        double startingVelo = 4.6;
+        double endingVelo = 11.5;
+        double time = 0.6;
+        double average;
+
+        average = ((endingVelo - startingVelo) / time);
+        System.out.println("a = " + average);
+
+    }
+}
