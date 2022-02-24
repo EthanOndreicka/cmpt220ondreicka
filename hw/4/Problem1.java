6f425bdfc60e643db74d257e3f7e16169af9f7d3
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2022-02-24 10:17:03 -0500

    hw

---
+++ hw/4/Problem1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Problem1 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of sides: ");
+        int numOfSides = input.nextInt();
+        System.out.print("Enter the side: ");
+        double length = input.nextDouble();
+
+        double area = ((numOfSides * (Math.pow(length, 2))) / (4 * Math.tan(3.14159 / numOfSides)));
+
+        System.out.println("The area of the polygon is: " + area);
+
+    }
+}
---
+++ hw/4/Problem2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problem2 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of the sides: ");
+        int numOfSides = input.nextInt();
+        System.out.print("Enter the radius of the bounding circle: ");
+        double radius = input.nextDouble();
+
+        
+
+    }
+}
