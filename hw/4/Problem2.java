d145d50c5d93acba723e602be4e2fe7cd8313483
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2022-02-24 10:45:11 -0500

    hw

--- hw/4/Problem2.java
+++ hw/4/Problem2.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 public class Problem2 {
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the number of the sides: ");
-        int numOfSides = input.nextInt();
+        System.out.print("Enter the number of sides: ");
+        int sides = input.nextInt();
         System.out.print("Enter the radius of the bounding circle: ");
         double radius = input.nextDouble();
-
-        
-
-    }
+       
+        System.out.println("The coordinates of the points on the polygon are");
+        for (int i = 0; i < sides; i++) {
+            double x = radius * Math.cos(2.0 * 3.14159 / sides * i);
+            double y = radius * Math.sin(2.0 * 3.14159 / sides * i);
+            System.out.print("(");
+            System.out.printf("%.2f", x);
+            System.out.print(", ");
+            System.out.printf("%.2f",y);
+            System.out.print(")");
+            System.out.println();
+            }
+       
+          }
+       
 }
