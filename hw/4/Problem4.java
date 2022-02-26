f1892f4fc98ebdc2f137447265e531c5d9a62cd0
Ethan Ondreicka <ethan.ondreicka1@marist.edu>
2022-02-25 22:15:14 -0500

    hw

---
+++ hw/4/Problem4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problem4 {
+    public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a positive integer: ");
+		int num = input.nextInt();
+
+		
+
+	}
+    
+}
+
