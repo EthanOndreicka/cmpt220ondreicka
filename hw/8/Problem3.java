c3c8e81bf10483405fb69faa26f177259e3fc461
Ethan Ondreicka <97995036+EthanOndreicka@users.noreply.github.com>
2022-04-15 21:50:15 -0400

    hw

--- hw/8/Problem3.java
+++ hw/8/Problem3.java
@@ -1,4 +1,4 @@
-// Ethan Ondreicka // April 14th 2022 // Professor Arias //
+// Ethan Ondreicka // April 14th, 2022 // Professor Arias //
 public class Problem3{
     public static void main(String[] args) {
         
